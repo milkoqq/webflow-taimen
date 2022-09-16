8bc6b8864ba7e5ca3af65054c730661a06c90ce4
milkoqq <theofanous.dim@gmail.com>
2022-09-17 02:04:45 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,21 +4,41 @@ import { gsap } from 'gsap'
 // <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.1/gsap.min.js"></script>
 // <script src="https://jn3rn6.csb.app/index.js"></script>
 
-let tl = gsap.timeline()
+const btnMenuOpen = document.querySelector('.nav_hamburger-wrap')
+const btnMenuClose = document.querySelector('.nav_expand-hamburger-close')
 
-tl.from('.nav_expand', {
-    height: '0vh',
-    duration: 0.5
-})
-    .to('.nav', {
-        borderRadius: '20px 20px 0px 0px',
-        duration: 1,
-        scaleX: 0.95
+let tl = gsap.timeline({ paused: true })
+
+    .from('.nav_expand', {
+        height: '0vh',
+        duration: 0.7,
+        ease: "power3.Out"
     })
+    .to('.main-wrapper', {
+        borderRadius: '20px 20px 0px 0px',
+        duration: 0.8,
+        scaleX: 0.97,
+        backgroundColor: 'rgb(255, 255, 255, 0.6)',
+        ease: "power1.inOut"
+    }, "<")
     .from('.nav_expand-link-text', {
-
         opacity: 0,
         yPercent: 100,
-        stagger: 0.05
-    })
+        stagger: 0.04,
+        ease: "power3.out"
+    }, '-=0.5')
+    .from('.is-gsap-nav', {
+        opacity: 0,
+        ease: "power0.out",
+    }, '-=0.4')
 
+
+
+
+btnMenuOpen.addEventListener('click', () => {
+    tl.play()
+})
+
+btnMenuClose.addEventListener('click', () => {
+    tl.reverse()
+})
